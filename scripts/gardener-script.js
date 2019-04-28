13a4de96d68e71bf816177c1470c00d3d4bfac02
LydiaAcienceAndArt <lydiali@caltech.alumni.edu>
2019-04-27 21:32:09 -0700

    test

---
+++ scripts/gardener-script.js
@@ -0,0 +1,35 @@
+var imageSlideIndex = 1;
+showSlides(imageSlideIndex);
+
+function plusSlides(x) {
+    showSlides(imageSlideIndex += x);
+}
+
+function selectCurrentSlide(x) {
+    showSlides(imageSlideIndex = x);
+}
+
+function showSlides(x) {
+    var i;
+    var slides = document.getElementsByClassName("imageSlides");
+    var dots = document.getElementsByClassName("select-dot");
+
+    if (x > slides.length) {
+        imageSlideIndex = 1;
+    }
+
+    if (x < 1) {
+        imageSlideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[imageSlideIndex - 1].style.display = "block";
+    dots[imageSlideIndex - 1].className += " active";
+}
