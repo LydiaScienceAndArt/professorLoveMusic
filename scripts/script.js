ed818573de4cc0d50aa2b457f343cddf3d468da9
LydiaAcienceAndArt <lydiali@caltech.alumni.edu>
2019-05-30 15:31:49 -0700

    mobile dev

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,10 @@
 function openLinkFromNavigationBar() {
     window.open("home.html","_self");
 }
+
+let mainNaviToggle = document.getElementById('main-navi-toggle');
+let sidebarLeft = document.getElementById('main-navi-bar');
+
+mainNaviToggle.addEventListener('click', function() {
+    window.open("mobile-navi-bar.html");
+});
