88733f1befe9b6e1595dbb0b5f4f3db271669077
LydiaAcienceAndArt <lydiali@caltech.alumni.edu>
2019-06-05 14:50:44 -0700

    mobile develop

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,9 @@ let mainNaviToggle = document.getElementById('main-navi-toggle');
 let sidebarLeft = document.getElementById('main-navi-bar');
 
 if (mainNaviToggle) {
+    // debug below:
+    console.log("inside!!");
+
     mainNaviToggle.addEventListener('click', function() {
         window.open("mobile-navi-bar.html");
     });
